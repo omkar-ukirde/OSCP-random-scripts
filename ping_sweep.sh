7a4aba255a63196841d4721823b111092b655bbd
omkar ukirde <10123675+omkar-ukirde@users.noreply.github.com>
2018-03-12 10:36:07 +0530

    Update ping_sweep.sh

--- ping_sweep.sh
+++ ping_sweep.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-for i in `seq 10 18`;
-do 
-	ping 192.168.56.$i -c 1 -W 1 >/dev/null && echo 192.168.56.$i
-done	
+for ip in $(seq 1 254); do 
+ping -c1  192.168.0.$ip | grep "bytes from" | cut -d" " -f4 | cut -d":" -f1 & 
+done
