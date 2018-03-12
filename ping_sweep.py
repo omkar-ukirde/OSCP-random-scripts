d608dd8d525b43fdc200d743770de64a2e3f6040
omkar ukirde <10123675+omkar-ukirde@users.noreply.github.com>
2018-03-12 12:16:21 +0530

    Update ping_sweep.py

--- ping_sweep.py
+++ ping_sweep.py
@@ -1,10 +1,12 @@
-import subprocess
-import os
-
-FNULL = open(os.devnull, 'w')
-for ping in range(1,25):
+from commands import getoutput
+from multiprocessing import Process
+def pingdef(ping):
 	address = "192.168.0." + str(ping)
-	res = subprocess.call(['ping', '-c', '1','-w','1', address] ,stdout=FNULL, stderr=FNULL)
-	if res == 0:
-      		print address
-   
+	line = getoutput("ping -n -c 1 -w 1 %s 2>/dev/null" % address)
+	if ('100% packet loss') not in line:
+		print address
+		
+for ping in range(1,254):
+	pingone = Process(target=pingdef,args=(ping,))
+	pingone.start()   
+	 
